70125690dd0143dd734a770e5e99a9ae921a71cc
rorthomas <rorthomas@66ec8c48-a24b-4d9a-a200-91c837921668>
2009-02-10 09:06:12 +0000

    added linux start scripts

---
+++ build/bin/release/linux/rorconfig.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+BASEDIR=${0%/*}
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$BASEDIR/"
+$BASEDIR/rorconfig $@
+
+# this should fix the hanging keys problem
+if [[ "$(which xset)" != "" ]] 
+then
+ xset r on
+else
+ echo "please install 'xset' in order to fix the key repeat bug that occurs sometimes. Sorry for the workaround."
+fi
