d1810523279ebf402c7dd10cb3c4f1bd57d84629
rorthomas <rorthomas@66ec8c48-a24b-4d9a-a200-91c837921668>
2009-05-04 18:17:10 +0000

    improved code: removed exit() removed resource file for windows from non-windows builds

--- build/scriptcompiler/main.cpp
+++ build/scriptcompiler/main.cpp
@@ -5,12 +5,15 @@
 void usage(char *name)
 {
 	printf("usage: %s <filename>", name);
-	exit(1);
 }
 
 int main(int argc, char *argv[])
 {
-	if(argc != 2) usage(argv[0]);
+	if(argc != 2) 
+	{
+		usage(argv[0]);
+		return 1;
+	}
 	ScriptEngine *se = new ScriptEngine();
 	return se->loadFile(argv[1]);
 	return 0;
