dc56fa19a50c4d0bd94fd326d8d3adb37d853e19
rorthomas <rorthomas@66ec8c48-a24b-4d9a-a200-91c837921668>
2009-06-16 17:31:40 +0000

    skidmarks improved, WIP, disabled by default

--- build/main/source/Skidmark.cpp
+++ build/main/source/Skidmark.cpp
@@ -24,156 +24,113 @@ along with Rigs of Rods.  If not, see <http://www.gnu.org/licenses/>.
 #include <cmath>
 
 using namespace Ogre;
+int Skidmark::instancecounter = 0;
 
-enum {
-  POSITION_BINDING,
-  TEXCOORD_BINDING
-};
-
-Skidmark::Skidmark(SceneManager *scm, OperationType opType) : scm(scm), pos(0)
+Skidmark::Skidmark(SceneManager *scm, SceneNode *snode, int _lenght) : scm(scm), mNode(snode), lenght(_lenght)
 {
-	initialize(opType,false);
-	setMaterial("BaseWhiteNoLighting");
 	mDirty = true;
-
-	mNode = scm->getRootSceneNode()->createChildSceneNode();
-	mNode->attachObject(this);
-	
-	for(int i=0; i<100; i++)
-		addPoint(Vector3::ZERO);
-	update();
 }
 
 Skidmark::~Skidmark()
 {
 }
 
-void Skidmark::setOperationType(OperationType opType)
+void Skidmark::addObject(Vector3 start)
 {
-  mRenderOp.operationType = opType;
+	LogManager::getSingleton().logMessage("new skidmark section");
+	skidmark_t skid;
+	skid.pos=0;
+	skid.lastPoint=start;
+	skid.colour = ColourValue(Math::RangeRandom(0, 100)/100.0f, Math::RangeRandom(0, 100)/100.0f, Math::RangeRandom(0, 100)/100.0f);
+
+	skid.points.resize(lenght);	
+	skid.obj = scm->createManualObject("skidmark" + StringConverter::toString(instancecounter++));
+	skid.obj->setDynamic(true);
+	skid.obj->setRenderingDistance(800);
+	skid.obj->begin("tracks/skidmark", RenderOperation::OT_LINE_STRIP); //OT_LINE_STRIP);
+	for(int i = 0; i < lenght; i++)
+	{
+		skid.points[i] = start;
+		skid.obj->position(start);
+	}
+    skid.obj->end();
+	mNode->attachObject(skid.obj);
+
+
+	objects.push(skid);
+
+	limitObjects();
 }
 
-RenderOperation::OperationType Skidmark::getOperationType() const
+void Skidmark::limitObjects()
 {
-  return mRenderOp.operationType;
+	if(objects.size()>5)
+	{
+		LogManager::getSingleton().logMessage("deleting first skidmarks section to keep the limits");
+		objects.front().points.clear();
+		scm->destroyManualObject(objects.front().obj);
+		objects.pop();
+	}
 }
 
-void Skidmark::addPoint(const Vector3 &p)
+void Skidmark::setPointInt(unsigned short index, const Vector3 &value)
 {
-   mPoints.push_back(p);
-   mDirty = true;
-}
-void Skidmark::addPoint(Real x, Real y, Real z)
-{
-   mPoints.push_back(Vector3(x,y,z));
-   mDirty = true;
-}
-const Vector3& Skidmark::getPoint(unsigned short index) const
-{
-   assert(index < mPoints.size() && "Point index is out of bounds!!");
-   return mPoints[index];
-}
-unsigned short Skidmark::getNumPoints(void) const
-{
-  return (unsigned short)mPoints.size();
+	objects.back().points[index] = value;
+	mDirty = true;
 }
 
-void Skidmark::setPoint(unsigned short index, const Vector3 &value)
+void Skidmark::setPoint(const Vector3 &value)
 {
-  assert(index < mPoints.size() && "Point index is out of bounds!!");
+	float minDistance = 0.05f;
+	float maxDistance = 0.5f;
 
-  mPoints[index] = value;
-  mDirty = true;
-}
 
-void Skidmark::setPoint2(const Vector3 &value)
-{
-	if(pos>=99)
-		pos=0;
-	setPoint(pos++, value);
-}
+	// far enough for new section?
+	if(!objects.size())
+	{
+		addObject(value);
+	} else
+	{
+		skidmark_t skid = objects.back();
+		// too near to update?
+		if(fabs(skid.lastPoint.distance(value)) < minDistance) return;
+		
+		// far enough for new section?
+		if((skid.lastPoint != Vector3::ZERO && fabs(skid.lastPoint.distance(value)) > maxDistance) || skid.pos >= skid.points.size())
+			addObject(value);
+	}
 
-void Skidmark::clear()
-{
-  mPoints.clear();
-  mDirty = true;
+	setPointInt(objects.back().pos, value);
+	objects.back().pos++;
+	objects.back().lastPoint = value;
 }
 
 void Skidmark::update()
 {
-  if (mDirty) fillHardwareBuffers();
-  //mNode->needUpdate();
+	skidmark_t skid = objects.back();
+	Vector3 vaabMin = skid.points[0];
+	Vector3 vaabMax = skid.points[0];
+	skid.obj->beginUpdate(0);
+	for(int i = 0; i < lenght; i++)
+	{
+		skid.obj->position(skid.points[i]);
+		skid.obj->colour(skid.colour);
+		if(skid.points[i].x < vaabMin.x) vaabMin.x = skid.points[i].x;
+		if(skid.points[i].y < vaabMin.y) vaabMin.y = skid.points[i].y;
+		if(skid.points[i].z < vaabMin.z) vaabMin.z = skid.points[i].z;
+		if(skid.points[i].x > vaabMax.x) vaabMax.x = skid.points[i].x;
+		if(skid.points[i].y > vaabMax.y) vaabMax.y = skid.points[i].y;
+		if(skid.points[i].z > vaabMax.z) vaabMax.z = skid.points[i].z;
+	}
+    skid.obj->end();
+
+	//manual->setBoundingBox(AxisAlignedBox(vaabMin, vaabMax));
+	
+	// Use infinite AAB to always stay visible
+	AxisAlignedBox aabInf;
+	aabInf.setInfinite();
+	skid.obj->setBoundingBox(aabInf);
+
+	mDirty = false;
 }
 
-void Skidmark::createVertexDeclaration()
-{
-  VertexDeclaration *decl = mRenderOp.vertexData->vertexDeclaration;
-  decl->addElement(POSITION_BINDING, 0, VET_FLOAT3, VES_POSITION);
-}
-
-void Skidmark::fillHardwareBuffers()
-{
-  int size = mPoints.size();
-
-  prepareHardwareBuffers(size,0);
-
-  if (!size) { 
-	mBox.setExtents(Vector3::ZERO,Vector3::ZERO);
-	mDirty=false;
-	return;
-  }
-  
-  Vector3 vaabMin = mPoints[0];
-  Vector3 vaabMax = mPoints[0];
-
-  HardwareVertexBufferSharedPtr vbuf =
-	mRenderOp.vertexData->vertexBufferBinding->getBuffer(0);
-
-  Real *prPos = static_cast<Real*>(vbuf->lock(HardwareBuffer::HBL_DISCARD));
-  {
-   for(int i = 0; i < size; i++)
-   {
-	  *prPos++ = mPoints[i].x;
-	  *prPos++ = mPoints[i].y;
-	  *prPos++ = mPoints[i].z;
-
-	  if(mPoints[i].x < vaabMin.x)
-		 vaabMin.x = mPoints[i].x;
-	  if(mPoints[i].y < vaabMin.y)
-		 vaabMin.y = mPoints[i].y;
-	  if(mPoints[i].z < vaabMin.z)
-		 vaabMin.z = mPoints[i].z;
-
-	  if(mPoints[i].x > vaabMax.x)
-		 vaabMax.x = mPoints[i].x;
-	  if(mPoints[i].y > vaabMax.y)
-		 vaabMax.y = mPoints[i].y;
-	  if(mPoints[i].z > vaabMax.z)
-		 vaabMax.z = mPoints[i].z;
-   }
-  }
-  vbuf->unlock();
-
-  mBox.setExtents(vaabMin, vaabMax);
-
-  mDirty = false;
-}
-
-/*
-void Skidmark::getWorldTransforms(Matrix4 *xform) const
-{
-   // return identity matrix to prevent parent transforms
-   *xform = Matrix4::IDENTITY;
-}
-*/
-/*
-const Quaternion &Skidmark::getWorldOrientation(void) const
-{
-   return Quaternion::IDENTITY;
-}
-
-const Vector3 &Skidmark::getWorldPosition(void) const
-{
-   return Vector3::ZERO;
-}
-*/
