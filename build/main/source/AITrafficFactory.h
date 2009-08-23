bd457dff3b78addb3d12375fd98d3a872e87a2ab
rorthomas <rorthomas@66ec8c48-a24b-4d9a-a200-91c837921668>
2009-08-23 11:13:23 +0000

    improved net code

--- build/main/source/AITrafficFactory.h
+++ build/main/source/AITrafficFactory.h
@@ -1,4 +1,25 @@
+/*
+This source file is part of Rigs of Rods
+Copyright 2005,2006,2007,2008,2009 Pierre-Michel Ricordel
+Copyright 2007,2008,2009 Thomas Fischer
+
+For more information, see http://www.rigsofrods.com/
+
+Rigs of Rods is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, as
+published by the Free Software Foundation.
+
+Rigs of Rods is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Rigs of Rods.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifdef AITRAFFIC
+
 #ifndef AITRAFFICFACTORY_H
 #define AITRAFFICFACTORY_H
 
@@ -23,11 +44,11 @@ class AITrafficFactory  : public StreamableFactory
 	protected:
 		Network *net;
 		Ogre::SceneManager *scm;
-	
+
 		// functions used by friends
 		void netUserAttributesChanged(int source, int streamid);
 		void localUserAttributesChanged(int newid);
 };
 
-#endif
-#endif
+#endif //AITRAFFICFACTORY_H
+#endif //AITRAFFIC
