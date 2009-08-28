f28293240744df272812abd405f14eb1627bc8de
rorthomas <rorthomas@66ec8c48-a24b-4d9a-a200-91c837921668>
2009-08-28 11:33:42 +0000

    improved package script

--- trunk/build/pack.sh
+++ trunk/build/pack.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# this is a utility script which renames and moves the resulting package
 make package
 mv ror-*-Linux-i686.tar.gz $1/ror-r$2-Linux-i686.tar.gz
+@echo $1/ror-r$2-Linux-i686.tar.gz
