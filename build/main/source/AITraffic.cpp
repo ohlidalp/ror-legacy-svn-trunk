5b80953eef27a66c9a6849444d55c0eb460514f1
imrenagy <imrenagy@66ec8c48-a24b-4d9a-a200-91c837921668>
2009-08-28 21:44:33 +0000

    - removed VTC related codes - added AITraffic executors

--- build/main/source/AITraffic.cpp
+++ build/main/source/AITraffic.cpp
@@ -23,6 +23,10 @@ AITraffic::~AITraffic()
 {
 }
 
+void AITraffic::sendStreamSetup()
+{
+}
+
 void AITraffic::sendStreamData()
 {
 /*
@@ -39,12 +43,48 @@ void AITraffic::sendStreamData()
 
 void AITraffic::receiveStreamData(unsigned int &type, int &source, unsigned int &streamid, char *buffer, unsigned int &len)
 {
-	memcpy(&nettraffic, buffer, sizeof(nettraffic_t));
+//	memcpy(&nettraffic, buffer, sizeof(nettraffic_t));
+	int a;
+	memcpy(&a, buffer, sizeof(int));
+	switch (a)
+		{
+			case AIMSG_PING:				ping(buffer, len)				;		break;
+			case AIMSG_SETUPVEHICLES:		setupVehicles(buffer, len)		;		break;
+			case AIMSG_SETPOSITIONDATA:		setPositionData(buffer, len)	;		break;
+			case AIMSG_SETUPLAMPS:			setupLamps(buffer, len)			;		break;
+			case AIMSG_SETUPSIGNS:			setupSigns(buffer, len)			;		break;
+			case AIMSG_SETUPZONES:			setupZones(buffer, len)			;		break;
+			case AIMSG_SETUPPORTALS:		setupPortals(buffer, len)		;		break;
+			case AIMSG_UPDATELAMPPROGRAMS:	updateLampPrograms(buffer, len)	;		break;	
+		}
 }
 
-void AITraffic::sendStreamSetup()
-{
-}
+/* --- AITraffic related executors -- */
+
+void AITraffic::ping(char *buffer, unsigned int &len)
+{}
+
+void AITraffic::setupVehicles(char *buffer, unsigned int &len)
+{}
+
+void AITraffic::setPositionData(char *buffer, unsigned int &len)
+{}
+
+void AITraffic::setupLamps(char *buffer, unsigned int &len)
+{}
+
+void AITraffic::setupSigns(char *buffer, unsigned int &len)
+{}
+
+void AITraffic::setupZones(char *buffer, unsigned int &len)
+{}
+
+void AITraffic::setupPortals(char *buffer, unsigned int &len)
+{}
+
+void AITraffic::updateLampPrograms(char *buffer, unsigned int &len)
+{}
+
 
 
 #endif //AITRAFFIC
