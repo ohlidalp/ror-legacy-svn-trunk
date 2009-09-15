24ae396fb7199bf51237b58ecc7be7db7e1a7317
rorthomas <rorthomas@66ec8c48-a24b-4d9a-a200-91c837921668>
2009-09-15 12:56:05 +0000

    improved syncing: now separated into steps, more clear. basically working. next: user interface improvements

--- trunk/build/installer/cevent.h
+++ trunk/build/installer/cevent.h
@@ -19,9 +19,11 @@ public:
 	// You *must* copy here the data to be transported
 	MyStatusEvent(const MyStatusEvent &event) : wxCommandEvent(event)
 	{
-		this->setText(event.getText());
-		this->setProgress(0, event.getProgress(0));
-		this->setProgress(1, event.getProgress(1));
+		this->text=event.text;
+		this->progress=event.progress;
+		this->filesToGo=event.filesToGo;
+		this->filesDone=event.filesDone;
+		this->speed=event.speed;
 	}
  
 	// Required for sending with wxPostEvent()
@@ -30,17 +32,15 @@ public:
 		return new MyStatusEvent(*this);
 	}
  
-	// data getter/setter
-	wxString getText() const { return m_text; }
-	void setText( const wxString& text ) { m_text = text; }
-	
-	float getProgress(int num) const { assert(num<2); return m_progress[num]; }
-	void setProgress(int num, float value) { assert(num<2); m_progress[num] = value; }
- 
-private:
 	// data members
-	float m_progress[2];
-	wxString m_text;
+	float progress;
+	wxString text;
+
+	int filesToGo;
+	int filesDone;
+	float speed;
+
+private:
 };
  
 typedef void (wxEvtHandler::*MyStatusEventFunction)(MyStatusEvent &);
@@ -68,6 +68,6 @@ typedef void (wxEvtHandler::*MyStatusEventFunction)(MyStatusEvent &);
 // named IDs in an enumeration.
 
 // MSE = my status event ;)
-enum { MSE_STARTING, MSE_UPDATING, MSE_DONE, MSE_ERROR };
+enum { MSE_STARTING, MSE_UPDATE1, MSE_UPDATE2, MSE_UPDATE3, MSE_DONE, MSE_ERROR };
 
 #endif //CEVENT_H__
