fea58710240a70a245a81ccaeda5cd6671426acc
rorthomas <rorthomas@66ec8c48-a24b-4d9a-a200-91c837921668>
2009-12-30 01:41:25 +0000

    added new runtime for VS 2008 professional added simple build script, which should make it easier to compile this

---
+++ tools/windows/msitools/build.bat
@@ -0,0 +1,3 @@
+"%PROGRAMFILES%\Windows Installer XML v3\bin\candle.exe" VCCRT4.wxs -out VCCRT4.wixobj
+"%PROGRAMFILES%\Windows Installer XML v3\bin\light.exe" VCCRT4.wixobj -out VCCRT4.msi   
+pause
