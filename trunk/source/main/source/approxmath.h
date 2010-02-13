a0932d0701274b7c29cf331ed03a30b443a09052
estamato <estamato@66ec8c48-a24b-4d9a-a200-91c837921668>
2010-02-13 00:25:20 +0000

    Clean up. Added fast random float functions (frand_*)

--- trunk/source/main/source/approxmath.h
+++ trunk/source/main/source/approxmath.h
@@ -8,101 +8,110 @@
 #ifndef __APPROXMATH_H__
 #define	__APPROXMATH_H__
 
+static int mirand = 1;
+
+// Returns a random number in the range [0, 1]
+inline float frand_01()
+{
+    unsigned int a;
+
+    mirand *= 16807;
+
+    a = (mirand&0x007fffff) | 0x40000000;
+
+    return( *((float*)&a) - 2.0f )*0.5f;
+}
+
+// Returns a random number in the range [0, 2]
+inline float frand_02()
+{
+    unsigned int a;
+
+    mirand *= 16807;
+
+    a = (mirand&0x007fffff) | 0x40000000;
+
+    return( *((float*)&a) - 2.0f );
+}
+
+// Returns a random number in the range [-1, 1]
+inline float frand_11()
+{
+    unsigned int a;
+
+    mirand *= 16807;
+
+    a = (mirand&0x007fffff) | 0x40000000;
+
+    return( *((float*)&a) - 3.0f );
+}
+
 // Calculates approximate e^x.
 // Use it in code not requiring precision
 inline float approx_exp (const float x)
 {
-	union
-	{
-	  float f;
-	  int i;
-	} _eco;
-
-  if (x < -15)
-  return 0. ;
-   else if (x > 88)
-     return 1e38 ;
-   else
-	_eco.i=12102203*x+1064652319;
-	return _eco.f;
+	if (x < -15)
+		return 0.f ;
+	else if (x > 88)
+		return 1e38f ;
+	else {
+		int i=12102203*x+1064652319;
+		return *(float *)&i;
+	}
 }
 
 // Calculates approximate 2^x
 // Use it in code not requiring precision
 inline float approx_pow2(const float x)
 {
-	union
-	{
-	  float f;
-	  int i;
-	} _eco;
+    int i = 8388608*x+1065353216;
 
-	 _eco.i=8388608*x+1065353216;
-     return _eco.f;
+    return *(float *)&i;
 }
 
 // Calculates approximate x^y
 // Use it in code not requiring precision
-inline float approx_pow (const float x, const float y)
+inline float approx_pow(const float x, const float y)
 {
-	union
-	{
-	  float f;
-	  int i;
-	} _eco;
+    float v = x;
+    int i = y * ( (*(int *)&v) - 1065353216) + 1065353216;
 
-	_eco.f=x;
-	_eco.i=y*(_eco.i-1065353216) + 1065353216;
-	return _eco.f;
+    return *(float *)&i;
 }
 
 // Calculates approximate square_root(x)
 // Use it in code not requiring precision
-inline float approx_sqrt(const float x)
+inline float approx_sqrt(const float y)
 {
-    union
-    {
-      float f;
-      int i;
-    } _eco;
+    float f = y;
+    int i = (( (*(int *)&f) - 1065353216)>>1) + 1065353216;
 
-    _eco.f=x;
-    _eco.i=((_eco.i-1065353216)>>1) + 1065353216;
-    return _eco.f;
+    return *(float *)&i;
 }
 
 // Calculates approximate 1/square_root(x)
 // it is faster than fast_invSqrt BUT
 // use it in code not requiring precision
-inline float approx_invsqrt(const float x)
+inline float approx_invsqrt(const float y)
 {
-  union // get bits for floating value
-  {
-    float x;
-    int i;
-  } u;
-  u.x = x;
-  u.i = 0x5f3759df - (u.i >> 1);  // gives initial guess y0
-  return u.x;
+    float f = y;
+    int i = 0x5f3759df - ( (*(int *)&f) >> 1);
+
+    return *(float *)&i;
 }
 
 // This function is a classic 1/square_root(x)code
 // used by quake's game engine.
 // It is very fast and has enough precision
 // to drive a physics engine.
-inline float fast_invSqrt(const float x)
+inline float fast_invSqrt(const float v)
 {
-  const float xhalf = 0.5f*x;
+    float y = v;
+    int i = 0x5f3759df - ( (*(int *)&y) >>1);
+    y = *(float *)&i;
 
-  union // get bits for floating value
-  {
-    float x;
-    int i;
-  } u;
-  u.x = x;
-  u.i = 0x5f3759df - (u.i >> 1);  // gives initial guess y0
-  u.x=u.x*(1.5f - xhalf*u.x*u.x); // Newton step, repeating increases accuracy
-  return u.x;
+    y *= (1.5f - (0.5f * v * y * y));
+    return y;
 }
 
 // It calculates a fast and accurate square_root(x)
@@ -115,6 +124,8 @@ inline float sign(const float x)
 {
 	return (x > 0.0f) ? 1.0f : (x < 0.0f) ? -1.0f : 0.0f;
 }
+
+
 
 
 #endif	/* _APPROXMATH_H */
