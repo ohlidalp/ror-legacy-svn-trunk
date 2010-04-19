0765e3fb73e1102199e88645ee82c40f21cf3d4b
rorthomas <rorthomas@66ec8c48-a24b-4d9a-a200-91c837921668>
2010-04-19 22:28:40 +0000

    increased version number to 0.37 in sources

--- source/main/ExampleFrameListener.h
+++ source/main/ExampleFrameListener.h
@@ -125,7 +125,7 @@ class HDRListener;
 // $Rev$
 #define SVN_REVISION "$Rev$"
 #define SVN_ID "$Id$"
-#define ROR_VERSION_STRING "0.36.3"
+#define ROR_VERSION_STRING "0.37"
 
 using namespace Ogre;
 
