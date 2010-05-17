5daa18028d5d577afe7d7b9b24e01961cbb6e030
rorthomas <rorthomas@66ec8c48-a24b-4d9a-a200-91c837921668>
2010-05-17 20:24:55 +0000

    updated doc build script

--- trunk/doc/updateDoc.sh
+++ trunk/doc/updateDoc.sh
@@ -3,14 +3,19 @@
 # update trunk first
 BASE=/var/svn-co/trunk
 WEBDIR=/var/www/docs.rigsofrods.com/htdocs
-INDEX=$WEBDIR/index.html
+INDEX=${WEBDIR}/index.html
+INDEX2=${WEBDIR}/index2.html
+ZIPFN=ror-doc.zip
+ZIP=${WEBDIR}/${ZIPFN}
 # we need to cd, since doxygen paths work locally
 cd $BASE
 
-#/usr/bin/svn up $BASE 2>&1 >>/dev/null
+/usr/bin/svn up $BASE #2>&1 >>/dev/null
 
-echo "<html><body><h2>Rigs of Rods Documentation overview</h2><ul>" > $INDEX
-chown lighttpd:lighttpd $INDEX
+echo "the documentation is currently being updated, please check back in some minutes" > $INDEX
+
+echo "<html><body><h2>Rigs of Rods Documentation overview</h2><ul>" > $INDEX2
+chown lighttpd:lighttpd $INDEX $INDEX2
 
 CONFIGS=$(ls $BASE/doc/*.conf | grep ".linux.")
 for CONFIG in $CONFIGS
@@ -31,19 +36,43 @@ do
  # generate new content
  mkdir -p $BASE/$OUTDIR
  mkdir -p $WEBDIR/$NAME/
- echo "/usr/bin/doxygen $CONFIG"
- /usr/bin/doxygen $CONFIG 2>&1 > $WEBDIR/$NAME/doxygen.log
+ #echo "/usr/bin/doxygen $CONFIG"
+ /usr/bin/doxygen $CONFIG  > $WEBDIR/$NAME/doxygen_stdout.log 2>$WEBDIR/$NAME/doxygen_stderr.log
  
  # copy new contents
  cp -rp $BASE/$OUTDIR/html/* $WEBDIR/$NAME/
 
  # add to index
- echo "<li><a href='/$NAME/'>$PROJECT</a></li>" >> $INDEX
+ echo "
+ <li>
+  <a style='font-size:1.4em' href='/$NAME/'>$PROJECT</a><br/>
+  <span style='font-size:0.7em'>(doxygen build log: <a href=\"$NAME/doxygen_stdout.log\">stdout</a>, <a href=\"$NAME/doxygen_stderr.log\">stderr</a>)</span><br/>&nbsp;
+ </li>" >> $INDEX2
 done
+echo "</ul>" >> $INDEX2
 
-echo "</ul>" >> $INDEX
+
+
+# finish the index html site
+LASTCHANGE=$(svn info ${BASE} | grep Last)
+REV=$(svn info ${BASE} | grep Revision)
 DATET=$(date)
-echo "<small>last update: $DATET (this updates automatically weekly)</small></body></html>" >> $INDEX
+
+echo "
+download zipped version of this documentation: <a href='${ZIPFN}'>${ZIPFN}</a><br/>
+<pre>last update: $DATET
+svn trunk ${REV}
+${LASTCHANGE}
+</pre>
+
+(this updates automatically daily)</body></html>" >> $INDEX2
+
+mv $INDEX2 $INDEX
+
+# zip the archive
+cd ${WEBDIR}
+/usr/bin/zip -rqu ${ZIP} .
+
 
 # fix permissions
 chown -R lighttpd:lighttpd $WEBDIR/*
