62f11801675142d9b6def3f3a7491dea6ed7e37e
rorthomas <rorthomas@66ec8c48-a24b-4d9a-a200-91c837921668>
2010-06-21 00:53:24 +0000

    added buildbot scripts to the SVN

---
+++ tools/buildbot/clean.bat
@@ -0,0 +1,5 @@
+cd build
+"C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\devenv.exe" ^
+/clean ^
+release ^
+RoR.sln
---
+++ tools/buildbot/compile.bat
@@ -0,0 +1,7 @@
+cd build
+C:\Windows\Microsoft.NET\Framework\v3.5\msbuild.exe RoR.sln ^
+/t:Build ^
+/p:Configuration=Release ^
+/maxcpucount:2 ^
+/verbosity:d ^
+/nologo
