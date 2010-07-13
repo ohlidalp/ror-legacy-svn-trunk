86a1a4165bc81e24c8905863c3dc75e6947be32d
rorthomas <rorthomas@66ec8c48-a24b-4d9a-a200-91c837921668>
2010-07-13 13:06:38 +0000

    added check for correct ogre version

--- source/main/RoRPrerequisites.h
+++ source/main/RoRPrerequisites.h
@@ -20,6 +20,11 @@ along with Rigs of Rods.  If not, see <http://www.gnu.org/licenses/>.
 
 // created on 30th of April 2010 by Thomas Fischer
 
+// macro that checks for the correct ogre version and aborts compilation if not correct
+#if OGRE_VERSION < 0x010701
+#error You need at least Ogre version 1.7.1, older versions are not supported
+#endif
+
 #ifndef RORPREREQUISITES_H__
 #define RORPREREQUISITES_H__
 
