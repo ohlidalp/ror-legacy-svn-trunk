da7f98297608a74f73dcbc29d4b4d4bab2588779
rorthomas <rorthomas@66ec8c48-a24b-4d9a-a200-91c837921668>
2010-08-07 01:11:21 +0000

    improved beam timing debug mode

--- source/main/BeamStats.cpp
+++ source/main/BeamStats.cpp
@@ -19,7 +19,7 @@ along with Rigs of Rods.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "BeamStats.h"
 
-#ifdef FEAT_BEAMSTATS
+#ifdef FEAT_TIMING
 
 // thomas, 11th of March 2008
 
@@ -30,10 +30,64 @@ using namespace std;
 using namespace Ogre;
 
 BeamEngineStats *BeamEngineStats::myInstance = 0;
+Ogre::String BeamEngineStats::typeDescriptions[MAX_TIMINGS];
+Ogre::String BeamEngineStats::typeDescriptions_gfx[MAX_TIMINGS];
 
 BeamEngineStats::BeamEngineStats()
 {
 	enabled=true;
+	updateTimeGUI=0;
+	stats = OverlayManager::getSingleton().getOverlayElement("tracks/DebugBeamTiming/Text");
+
+	// setup descriptions
+	typeDescriptions[BES_CORE_WholeTruckCalc]    = "Sum";
+	typeDescriptions[BES_CORE_Beams]             = "Beams";
+	typeDescriptions[BES_CORE_Nodes]             = "Nodes";
+	typeDescriptions[BES_CORE_TruckEngine]       = "TruckEngine";
+	typeDescriptions[BES_CORE_Rigidifiers]       = "Rigidifiers";
+	typeDescriptions[BES_CORE_Ropes]             = "Ropes";
+	typeDescriptions[BES_CORE_Turboprop]         = "Turboprop";
+	typeDescriptions[BES_CORE_Screwprop]         = "Screwprop";
+	typeDescriptions[BES_CORE_Wing]              = "Wing";
+	typeDescriptions[BES_CORE_FuseDrag]          = "FuseDrag";
+	typeDescriptions[BES_CORE_Airbrakes]         = "Airbrakes";
+	typeDescriptions[BES_CORE_Buoyance]          = "Buoyance";
+	typeDescriptions[BES_CORE_Contacters]        = "Contacters";
+	typeDescriptions[BES_CORE_CollisionCab]      = "CollisionCab";
+	typeDescriptions[BES_CORE_Wheels]            = "Wheels";
+	typeDescriptions[BES_CORE_Shocks]            = "Shocks";
+	typeDescriptions[BES_CORE_Hydros]            = "Hydros";
+	typeDescriptions[BES_CORE_Commands]          = "Commands";
+	typeDescriptions[BES_CORE_AnimatedProps]     = "AnimatedProps";
+	typeDescriptions[BES_CORE_SkeletonColouring] = "SkeletonColouring";
+	typeDescriptions[BES_CORE_Hooks]             = "Hooks";
+	typeDescriptions[BES_CORE_SlideNodes]        = "SlideNodes";
+	typeDescriptions[BES_CORE_Axles]             = "Axles";
+	typeDescriptions[BES_CORE_Replay]            = "Replay";
+	typeDescriptions[BES_CORE_Skidmarks]         = "Skidmarks";
+
+
+	typeDescriptions_gfx[BES_GFX_UpdateSkeleton]            = "UpdateSkeleton";
+	typeDescriptions_gfx[BES_GFX_ScaleTruck]                = "ScaleTruck";
+	typeDescriptions_gfx[BES_GFX_checkBeamMaterial]         = "checkBeamMaterial";
+	typeDescriptions_gfx[BES_GFX_pushNetwork]               = "pushNetwork";
+	typeDescriptions_gfx[BES_GFX_calcNetwork]               = "calcNetwork";
+	typeDescriptions_gfx[BES_GFX_calc_masses2]              = "calc_masses2";
+	typeDescriptions_gfx[BES_GFX_calcBox]                   = "calcBox";
+	typeDescriptions_gfx[BES_GFX_calcNodeConnectivityGraph] = "calcNodeConGraph";
+	typeDescriptions_gfx[BES_GFX_calculateDriverPos]        = "calculateDriverPos";
+	typeDescriptions_gfx[BES_GFX_framestep]                 = "framestep";
+	typeDescriptions_gfx[BES_GFX_sendStreamData]            = "sendStreamData";
+	typeDescriptions_gfx[BES_GFX_receiveStreamData]         = "receiveStreamData";
+	typeDescriptions_gfx[BES_GFX_calcAnimators]             = "calcAnimators";
+	typeDescriptions_gfx[BES_GFX_updateFlares]              = "updateFlares";
+	typeDescriptions_gfx[BES_GFX_updateProps]               = "updateProps";
+	typeDescriptions_gfx[BES_GFX_updateSoundSources]        = "updateSoundSources";
+	typeDescriptions_gfx[BES_GFX_updateVisual]              = "updateVisual";
+	typeDescriptions_gfx[BES_GFX_updateFlexBodies]          = "updateFlexBodies";
+	typeDescriptions_gfx[BES_GFX_updateNetworkInfo]         = "updateNetworkInfo";
+
+	stats->setCaption("calculating ...");
 }
 
 BeamEngineStats::~BeamEngineStats()
@@ -48,17 +102,26 @@ BeamEngineStats &BeamEngineStats::getInstance()
 	return *myInstance;
 }
 
-BeamThreadStats *BeamEngineStats::getClient(int number)
+BeamThreadStats *BeamEngineStats::getClient(int number, int type)
 {
-	if(enabled)
-	{
-		BeamThreadStats *bts = new BeamThreadStats();
-		if(statClients[number]!=0)
-			delete statClients[number];
-		statClients[number] = bts;
-		return bts;
-	} else
+	if(!enabled)
 		return 0;
+	BeamThreadStats *bts = new BeamThreadStats(type);
+
+	// TODO: check for duplicates
+	//if(statClients[number]!=0)
+	//	delete statClients[number];
+
+	stats_entry_t e;
+	e.trucknum = number;
+	e.stat = bts;
+
+	if(type == BES_CORE)
+		statClients.push_back(e);
+	else if(type == BES_GFX)
+		statClientsGFX.push_back(e);
+
+	return bts;
 }
 
 void BeamEngineStats::setup(bool enabled)
@@ -68,9 +131,11 @@ void BeamEngineStats::setup(bool enabled)
 
 bool BeamEngineStats::updateGUI(float dt, int current_truck, Beam **trucks)
 {
-	static OverlayElement *stats = 0;
-	if(!stats)
-		stats = OverlayManager::getSingleton().getOverlayElement("tracks/DebugBeamTiming/Text");
+	updateTimeGUI += dt;
+	if(updateTimeGUI < 5.0f)
+		return true;
+
+	updateTimeGUI = 0;
 	
 	if(statClients.size() == 0)
 		return true;
@@ -78,33 +143,79 @@ bool BeamEngineStats::updateGUI(float dt, int current_truck, Beam **trucks)
 	if(!stats->isVisible())
 		return true;
 
-	std::map<int, BeamThreadStats*>::iterator it;
 	String msg = "";
-	for(it=statClients.begin(); it!=statClients.end(); it++)
+
+	char tmp[1024]="";
+	char tmp_gfx[1024]="";
+	char line[1024]="";
+
+	for(int c = 0; c < statClients.size(); c++)
 	{
 		//if(it->first != current_truck || trucks[it->first]->state == SLEEPING)
-		if(trucks[it->first]->state == SLEEPING)
+		if(trucks[statClients[c].trucknum]->state == SLEEPING)
 			continue;
 
-		double sum=0, t=0, perc=0;
+		BeamThreadStats *core = statClients[c].stat;
+		BeamThreadStats *gfx  = statClientsGFX[c].stat;
 
-		msg += "\n";
-		msg += "Truck "+StringConverter::toString(it->first) + "\n";
-		msg += "  Graphic Frames: " + StringConverter::toString(it->second->getFramecount()) + "\n";
-		msg += "  Physic Frames: " + StringConverter::toString(it->second->getPhysFrameCount()) + "\n";
+		double sum=0, sum_gfx=0, t=0, perc=0;
+		msg += "Stats Timestep: 5 Seconds\n";
+		msg += "Truck "+StringConverter::toString(statClients[c].trucknum) + "\n";
+		msg += "  Graphic Frames: " + StringConverter::toString(core->getFramecount()) + "\n";
+		msg += "  Physic Frames:  " + StringConverter::toString(core->getPhysFrameCount()) + "\n";
+
+		sprintf(line, "%-46s  |  %-46s\n", "============== PHYSICS ==============", "================ GFX ================");
+		msg += String(line);
 		
-		sum = it->second->getTiming(BeamThreadStats::WholeTruckCalc);
+		sum = core->getTiming(BES_CORE_WholeTruckCalc);
+		sum_gfx = core->getTiming(BES_CORE_WholeTruckCalc);
 		if(sum==0)
 			continue;
 		
-		msg += "  sum: 100% ("+StringConverter::toString((Real)sum)+"s\n";
-		
+		sprintf(tmp, " %20s: % 10.4f%% : %1.6fs", "Whole truck", 100.0f, sum);
+		sprintf(tmp_gfx, " %20s: % 10.4f%% : %1.6fs", "FrameTime", 100.0f, sum_gfx);
+		sprintf(line, "%-46s  |  %-46s\n", tmp, tmp_gfx);
+		msg += String(line);
+
+		float sum_real = 0, sum_real_gfx = 0;
 		for(int i=1;i<MAX_TIMINGS;i++)
 		{
-			t = it->second->getTiming(i);
-			perc=(int)(100.0 * t / sum);
-			if(perc>=2)
-				msg += "  " + BeamThreadStats::typeDescriptions[i] + ": "+StringConverter::toString((int)perc)+"% ("+StringConverter::toString((Real)t)+"s)\n";
+			sprintf(tmp, "");
+			sprintf(tmp_gfx, "");
+
+			bool addLine=false;
+			if(!typeDescriptions[i].empty())
+			{
+				t = core->getTiming(i);
+				sum_real += t;
+				perc = (100.0 * t / sum);
+				sprintf(tmp, " %20s: % 10.4f%% : %1.6fs", typeDescriptions[i].c_str(), perc, t);
+				addLine=true;
+			}
+			if(!typeDescriptions_gfx[i].empty())
+			{
+				t = gfx->getTiming(i);
+				sum_real_gfx += t;
+				perc = (100.0 * t / sum_gfx);
+				sprintf(tmp_gfx, " %20s: % 10.4f%% : %1.6fs", typeDescriptions_gfx[i].c_str(), perc, t);
+				addLine=true;
+			}
+			if(!addLine) break;
+			sprintf(line, "%-46s  |  %-46s\n", tmp, tmp_gfx);
+			msg += String(line);
+
+		}
+		// find out the rest time
+		{
+			float unknown_time = sum - sum_real;
+			perc=(int)(100.0 * unknown_time / sum);
+			sprintf(tmp, " %20s: % 10.4f%% : %1.6fs", "Unknown", perc, unknown_time);
+
+			float unknown_time_gfx = sum_gfx - sum_real_gfx;
+			perc=(int)(100.0 * unknown_time_gfx / sum_gfx);
+			sprintf(tmp_gfx, " %20s: % 10.4f%% : %1.6fs", "Unknown", perc, unknown_time_gfx);
+			sprintf(line, "%-46s  |  %-46s\n", tmp, tmp_gfx);
+			msg += String(line);
 		}
 	}
 	stats->setCaption(msg);
@@ -114,10 +225,7 @@ bool BeamEngineStats::updateGUI(float dt, int current_truck, Beam **trucks)
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-Ogre::String BeamThreadStats::typeDescriptions[MAX_TIMINGS];
-
-BeamThreadStats::BeamThreadStats()
+BeamThreadStats::BeamThreadStats(int _type) : stype(_type)
 {
 	for (int i=0; i<MAX_TIMINGS; i++) 
 	{
@@ -127,26 +235,7 @@ BeamThreadStats::BeamThreadStats()
 	}
 	framecounter=0;
 	physcounter=0;
-
-	// setup descriptions
-	BeamThreadStats::typeDescriptions[WholeTruckCalc] = "Sum";
-	BeamThreadStats::typeDescriptions[Beams] = "Beams";
-	BeamThreadStats::typeDescriptions[Nodes] = "Nodes";
-	BeamThreadStats::typeDescriptions[TruckEngine] = "TruckEngine";
-	BeamThreadStats::typeDescriptions[Rigidifiers] = "Rigidifiers";
-	BeamThreadStats::typeDescriptions[Ropes] = "Ropes";
-	BeamThreadStats::typeDescriptions[Turboprop] = "Turboprop";
-	BeamThreadStats::typeDescriptions[Screwprop] = "Screwprop";
-	BeamThreadStats::typeDescriptions[Wing] = "Wing";
-	BeamThreadStats::typeDescriptions[FuseDrag] = "FuseDrag";
-	BeamThreadStats::typeDescriptions[Airbrakes] = "Airbrakes";
-	BeamThreadStats::typeDescriptions[Buoyance] = "Buoyance";
-	BeamThreadStats::typeDescriptions[Contacters] = "Contacters";
-	BeamThreadStats::typeDescriptions[CollisionCab] = "CollisionCab";
-	BeamThreadStats::typeDescriptions[Wheels] = "Wheels";
-	BeamThreadStats::typeDescriptions[Shocks] = "Shocks";
-	BeamThreadStats::typeDescriptions[Hydros] = "Hydros";
-	BeamThreadStats::typeDescriptions[Commands] = "Commands";
+	updateTime=0;
 }
 
 BeamThreadStats::~BeamThreadStats()
@@ -155,8 +244,13 @@ BeamThreadStats::~BeamThreadStats()
 
 void BeamThreadStats::frameStep(float ds)
 {
-	for (int i=0; i<MAX_TIMINGS; i++) 
-		timings[i]=0;
+	updateTime += ds;
+	if(updateTime > 5.0f)
+	{
+		for (int i=0; i<MAX_TIMINGS; i++)
+			timings[i]=0;
+		updateTime = 0;
+	}
 	framecounter++;
 }
 
@@ -172,10 +266,15 @@ void BeamThreadStats::queryStop(int type)
 
 	timings[type] += (timings_start[type]->elapsed());
 	
-	if(type == WholeTruckCalc)
+	if(stype == BES_CORE && type == BES_CORE_WholeTruckCalc)
 	{
 		// secure timings
 		physcounter++;
+		memcpy(savedTimings, timings, sizeof(double) * MAX_TIMINGS);
+	}
+	else if(stype == BES_GFX)
+	{
+		// secure timings
 		memcpy(savedTimings, timings, sizeof(double) * MAX_TIMINGS);
 	}
 }
@@ -196,5 +295,5 @@ double BeamThreadStats::getTiming(int type)
 }
 
 
-#endif
+#endif //FEAT_TIMING
 
