ec48b6f8335d706102b1d5a69d7fcd7aae811bae
rorthomas <rorthomas@66ec8c48-a24b-4d9a-a200-91c837921668>
2011-01-31 00:07:29 +0000

    MemoryWrapper working now: tracking deallocations as well

--- source/main/memoryWrapper.cpp
+++ source/main/memoryWrapper.cpp
@@ -3,17 +3,25 @@
 
 unsigned long MemoryWrapper::memory_used = 0;
 
+std::map<void *, unsigned long> MemoryWrapper::allocations;
+
 void *MemoryWrapper::allocBytes(size_t count)
 {
 	void* ptr = nedalloc::nedmalloc(count);
+
 	memory_used += count;
+	allocations[ptr] = count;
+
 	return ptr;
 }
 
 void *MemoryWrapper::callocBytes(size_t no, size_t size)
 {
 	void* ptr = nedalloc::nedcalloc(no, size);
+
 	memory_used += size;
+	allocations[ptr] = size;
+
 	return ptr;
 }
 
@@ -21,6 +29,13 @@ void MemoryWrapper::deallocBytes(void* ptr)
 {
 	if(!ptr)
 		return;
+
+	// get the memory size of this and substract it again
+	if(allocations.find(ptr) != allocations.end())
+	{
+		memory_used -= allocations[ptr];
+		allocations.erase(ptr);
+	}
 
 	nedalloc::nedfree(ptr);
 }
@@ -31,6 +46,7 @@ void* MemoryWrapper::allocBytesAligned(size_t align, size_t count)
 	void* ptr = align ? nedalloc::nedmemalign(align, count) : nedalloc::nedmemalign(SIMD_ALIGNMENT, count);
 
 	memory_used += count;
+	allocations[ptr] = count;
 	
 	return ptr;
 }
@@ -39,6 +55,13 @@ void MemoryWrapper::deallocBytesAligned(size_t align, void* ptr)
 {
 	if (!ptr)
 		return;
+
+	// get the memory size of this and substract it again
+	if(allocations.find(ptr) != allocations.end())
+	{
+		memory_used -= allocations[ptr];
+		allocations.erase(ptr);
+	}
 
 	nedalloc::nedfree(ptr);
 }
