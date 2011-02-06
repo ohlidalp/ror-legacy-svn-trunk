09972ed9567bdafb2acfa37bf893b2cc2db92d95
rorthomas <rorthomas@66ec8c48-a24b-4d9a-a200-91c837921668>
2011-02-06 01:29:57 +0000

    updated license texts

--- trunk/source/main/gfx/materialFunctionMapper.cpp
+++ trunk/source/main/gfx/materialFunctionMapper.cpp
@@ -1,12 +1,12 @@
 /*
 This source file is part of Rigs of Rods
-Copyright 2005,2006,2007,2008,2009 Pierre-Michel Ricordel
-Copyright 2007,2008,2009 Thomas Fischer
+Copyright 2005-2011 Pierre-Michel Ricordel
+Copyright 2007-2011 Thomas Fischer
 
 For more information, see http://www.rigsofrods.com/
 
 Rigs of Rods is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 3, as
+it under the terms of the GNU General Public License version 3, as 
 published by the Free Software Foundation.
 
 Rigs of Rods is distributed in the hope that it will be useful,
@@ -17,7 +17,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Rigs of Rods.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 #include "materialFunctionMapper.h"
 #include "Settings.h"
 
