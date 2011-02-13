02fdd1d34a32c6b74fec52e70ba8cf0e2670840c
rorthomas <rorthomas@66ec8c48-a24b-4d9a-a200-91c837921668>
2011-02-13 14:54:15 +0000

    more StringVector fixes

--- trunk/source/main/gameplay/TorqueCurve.h
+++ trunk/source/main/gameplay/TorqueCurve.h
@@ -23,6 +23,7 @@ along with Rigs of Rods.  If not, see <http://www.gnu.org/licenses/>.
 #include "RoRPrerequisites.h"
 #include "OgrePrerequisites.h"
 
+#include <OgreStringVector.h>
 
 /**
  * @file TorqueCurve.h
