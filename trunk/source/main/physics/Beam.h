be9240ed34ba7fe128f95682bd1beca9fd459f8e
rorlifter <rorlifter@66ec8c48-a24b-4d9a-a200-91c837921668>
2011-05-03 17:21:29 +0000

    Added: Hook options hookrange: how far to scan for a node to lock to speedcoef: pulling speed hookforce: force limit exceed -> no lock self-lock: can lock to my own truckTruckCollisions auto-lock: will lock automatically without any key pressed (using doupdate) hooktimer: timer to avoid autolock group#: groups activated by triggers
    Added: triggeroption "h"
    Enables a trigger to unlock a hookgroup
    todo: triggeroption "H" for a trigger to lock a hookgroup

--- trunk/source/main/physics/Beam.h
+++ trunk/source/main/physics/Beam.h
@@ -129,7 +129,7 @@ public:
 	void lightsToggle();
 	void tieToggle(int group=-1);
 	void ropeToggle(int group=-1);
-	void hookToggle(int group=-1);
+	void hookToggle(int group=-1, bool autolock = false, float timer = 0.0f);
 	void toggleSlideNodeLock();
 	void toggleCustomParticles();
 	void toggleAxleLock();	//! diff lock on or off
@@ -322,6 +322,7 @@ public:
 	bool isLocked();
 	int tsteps;
 	float avichatter_timer;
+	float autolock_timer;
 
 protected:
 
