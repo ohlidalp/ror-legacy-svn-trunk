e9484bf8f42f6d4413b20b2cf3493d83b376c641
rorlifter <rorlifter@66ec8c48-a24b-4d9a-a200-91c837921668>
2011-05-05 12:04:14 +0000

    Added Section: Lockgroups Added Setting: lockgroup_default_molock Removed: Nodesection parsing lockgroups

--- source/main/physics/BeamData.h
+++ source/main/physics/BeamData.h
@@ -148,8 +148,9 @@ enum TRUCK_SECTIONS {
 	BTS_COMMENT,
 	BTS_SECTION,
 	BTS_IN_SECTION,
-	BTS_HOOKS,
 	BTS_VIDCAM,
+	BTS_HOOKS,
+	BTS_LOCKGROUPS,
 	BTS_END,
 };
 
@@ -220,6 +221,7 @@ static const float HOOK_FORCE_DEFAULT		    = 10000000.0f;
 static const float HOOK_RANGE_DEFAULT           = 0.4f;
 static const float HOOK_SPEED_DEFAULT           = 0.00025f;
 static const float HOOK_LOCK_TIMER_DEFAULT      = 5.0;
+static const int   NODE_LOCKGROUP_DEFAULT		= -1; // all hooks scan all nodes
 /* Enumerations */
 enum {
 	THREAD_MONO,    //!< mono threading mode
