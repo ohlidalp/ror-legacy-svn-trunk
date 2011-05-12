5bc56c7b39056c980bc96962607424a112304164
rorthomas <rorthomas@66ec8c48-a24b-4d9a-a200-91c837921668>
2011-05-12 10:54:49 +0000

    fixed some things from the ogre wiki

--- source/main/gfx/DynamicLines.cpp
+++ source/main/gfx/DynamicLines.cpp
@@ -1,24 +1,5 @@
-/*
-This source file is part of Rigs of Rods
-Copyright 2005-2011 Pierre-Michel Ricordel
-Copyright 2007-2011 Thomas Fischer
-
-For more information, see http://www.rigsofrods.com/
-
-Rigs of Rods is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 3, as 
-published by the Free Software Foundation.
-
-Rigs of Rods is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Rigs of Rods.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// from Ogre wiki: http://www.ogre3d.org/tikiwiki/DynamicLineDrawing
 #include "DynamicLines.h"
-
 #include <Ogre.h>
 #include <cassert>
 #include <cmath>
@@ -26,15 +7,15 @@ along with Rigs of Rods.  If not, see <http://www.gnu.org/licenses/>.
 using namespace Ogre;
 
 enum {
-  POSITION_BINDING,
-  TEXCOORD_BINDING
+	POSITION_BINDING,
+	TEXCOORD_BINDING
 };
 
 DynamicLines::DynamicLines(char* materialname, OperationType opType)
 {
-  initialize(opType,false);
-  setMaterial(materialname);
-  mDirty = true;
+	initialize(opType,false);
+	setMaterial(materialname);
+	mDirty = true;
 }
 
 DynamicLines::~DynamicLines()
@@ -43,124 +24,120 @@ DynamicLines::~DynamicLines()
 
 void DynamicLines::setOperationType(OperationType opType)
 {
-  mRenderOp.operationType = opType;
+	mRenderOp.operationType = opType;
 }
 
 RenderOperation::OperationType DynamicLines::getOperationType() const
 {
-  return mRenderOp.operationType;
+	return mRenderOp.operationType;
 }
 
 void DynamicLines::addPoint(const Vector3 &p)
 {
-   mPoints.push_back(p);
-   mDirty = true;
+	mPoints.push_back(p);
+	mDirty = true;
 }
 void DynamicLines::addPoint(Real x, Real y, Real z)
 {
-   mPoints.push_back(Vector3(x,y,z));
-   mDirty = true;
+	mPoints.push_back(Vector3(x,y,z));
+	mDirty = true;
 }
 const Vector3& DynamicLines::getPoint(unsigned short index) const
 {
-   assert(index < mPoints.size() && "Point index is out of bounds!!");
-   return mPoints[index];
+	assert(index < mPoints.size() && "Point index is out of bounds!!");
+	return mPoints[index];
 }
 unsigned short DynamicLines::getNumPoints(void) const
 {
-  return (unsigned short)mPoints.size();
+	return (unsigned short)mPoints.size();
 }
 void DynamicLines::setPoint(unsigned short index, const Vector3 &value)
 {
-  assert(index < mPoints.size() && "Point index is out of bounds!!");
+	assert(index < mPoints.size() && "Point index is out of bounds!!");
 
-  mPoints[index] = value;
-  mDirty = true;
+	mPoints[index] = value;
+	mDirty = true;
 }
 void DynamicLines::clear()
 {
-  mPoints.clear();
-  mDirty = true;
+	mPoints.clear();
+	mDirty = true;
 }
 
 void DynamicLines::update()
 {
-  if (mDirty) fillHardwareBuffers();
+	if (mDirty) fillHardwareBuffers();
 }
 
 void DynamicLines::createVertexDeclaration()
 {
-  VertexDeclaration *decl = mRenderOp.vertexData->vertexDeclaration;
-  decl->addElement(POSITION_BINDING, 0, VET_FLOAT3, VES_POSITION);
+	VertexDeclaration *decl = mRenderOp.vertexData->vertexDeclaration;
+	decl->addElement(POSITION_BINDING, 0, VET_FLOAT3, VES_POSITION);
 }
 
 void DynamicLines::fillHardwareBuffers()
 {
-  int size = (int)mPoints.size();
+	int size = (int)mPoints.size();
 
-  prepareHardwareBuffers(size,0);
+	prepareHardwareBuffers(size,0);
 
-  if (!size) { 
-    mBox.setExtents(Vector3::ZERO,Vector3::ZERO);
-    mDirty=false;
-    return;
-  }
+	if (!size) { 
+	mBox.setExtents(Vector3::ZERO,Vector3::ZERO);
+	mDirty=false;
+	return;
+	}
   
-  Vector3 vaabMin = mPoints[0];
-  Vector3 vaabMax = mPoints[0];
+	Vector3 vaabMin = mPoints[0];
+	Vector3 vaabMax = mPoints[0];
 
-  HardwareVertexBufferSharedPtr vbuf =
-    mRenderOp.vertexData->vertexBufferBinding->getBuffer(0);
+	HardwareVertexBufferSharedPtr vbuf =
+	mRenderOp.vertexData->vertexBufferBinding->getBuffer(0);
 
-  //Real *prPos = static_cast<Real*>(vbuf->lock(HardwareBuffer::HBL_NORMAL));
-  Real *prPos=(Real*)malloc(vbuf->getSizeInBytes());
-  Real *orPos=prPos;
-  {
-   for(int i = 0; i < size; i++)
-   {
-      *prPos++ = mPoints[i].x;
-      *prPos++ = mPoints[i].y;
-      *prPos++ = mPoints[i].z;
+	Real *prPos = static_cast<Real*>(vbuf->lock(HardwareBuffer::HBL_DISCARD));
+	{
+	for(int i = 0; i < size; i++)
+	{
+		*prPos++ = mPoints[i].x;
+		*prPos++ = mPoints[i].y;
+		*prPos++ = mPoints[i].z;
 
-      if(mPoints[i].x < vaabMin.x)
-         vaabMin.x = mPoints[i].x;
-      if(mPoints[i].y < vaabMin.y)
-         vaabMin.y = mPoints[i].y;
-      if(mPoints[i].z < vaabMin.z)
-         vaabMin.z = mPoints[i].z;
+		if(mPoints[i].x < vaabMin.x)
+			vaabMin.x = mPoints[i].x;
+		if(mPoints[i].y < vaabMin.y)
+			vaabMin.y = mPoints[i].y;
+		if(mPoints[i].z < vaabMin.z)
+			vaabMin.z = mPoints[i].z;
 
-      if(mPoints[i].x > vaabMax.x)
-         vaabMax.x = mPoints[i].x;
-      if(mPoints[i].y > vaabMax.y)
-         vaabMax.y = mPoints[i].y;
-      if(mPoints[i].z > vaabMax.z)
-         vaabMax.z = mPoints[i].z;
-   }
-  }
-  vbuf->writeData(0, vbuf->getSizeInBytes(), orPos, true);
-  free(orPos);
-  //vbuf->unlock();
+		if(mPoints[i].x > vaabMax.x)
+			vaabMax.x = mPoints[i].x;
+		if(mPoints[i].y > vaabMax.y)
+			vaabMax.y = mPoints[i].y;
+		if(mPoints[i].z > vaabMax.z)
+			vaabMax.z = mPoints[i].z;
+	}
+	}
+	vbuf->unlock();
 
-  mBox.setExtents(vaabMin, vaabMax);
+	mBox.setExtents(vaabMin, vaabMax);
 
-  mDirty = false;
+	mDirty = false;
 }
 
 /*
 void DynamicLines::getWorldTransforms(Matrix4 *xform) const
 {
-   // return identity matrix to prevent parent transforms
-   *xform = Matrix4::IDENTITY;
+	// return identity matrix to prevent parent transforms
+	*xform = Matrix4::IDENTITY;
 }
 */
 /*
 const Quaternion &DynamicLines::getWorldOrientation(void) const
 {
-   return Quaternion::IDENTITY;
+	return Quaternion::IDENTITY;
 }
 
 const Vector3 &DynamicLines::getWorldPosition(void) const
 {
-   return Vector3::ZERO;
+	return Vector3::ZERO;
 }
 */
--- source/main/gfx/DynamicLines.h
+++ source/main/gfx/DynamicLines.h
@@ -1,22 +1,4 @@
-/*
-This source file is part of Rigs of Rods
-Copyright 2005-2011 Pierre-Michel Ricordel
-Copyright 2007-2011 Thomas Fischer
-
-For more information, see http://www.rigsofrods.com/
-
-Rigs of Rods is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 3, as 
-published by the Free Software Foundation.
-
-Rigs of Rods is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Rigs of Rods.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// from Ogre wiki: http://www.ogre3d.org/tikiwiki/DynamicLineDrawing
 #ifndef _DYNAMIC_LINES_H_
 #define _DYNAMIC_LINES_H_
 
@@ -26,59 +8,59 @@ along with Rigs of Rods.  If not, see <http://www.gnu.org/licenses/>.
 
 class DynamicLines : public DynamicRenderable
 {
-  typedef Ogre::Vector3 Vector3;
-  typedef Ogre::Quaternion Quaternion;
-  typedef Ogre::Camera Camera;
-  typedef Ogre::Real Real;
-  typedef Ogre::RenderOperation::OperationType OperationType;
+	typedef Ogre::Vector3 Vector3;
+	typedef Ogre::Quaternion Quaternion;
+	typedef Ogre::Camera Camera;
+	typedef Ogre::Real Real;
+	typedef Ogre::RenderOperation::OperationType OperationType;
 
 public:
-  /// Constructor - see setOperationType() for description of argument.
-  DynamicLines(char* materialname, OperationType opType=Ogre::RenderOperation::OT_LINE_STRIP);
-  virtual ~DynamicLines();
+	/// Constructor - see setOperationType() for description of argument.
+	DynamicLines(char* materialname, OperationType opType=Ogre::RenderOperation::OT_LINE_STRIP);
+	virtual ~DynamicLines();
 
-  /// Add a point to the point list
-  void addPoint(const Ogre::Vector3 &p);
-  /// Add a point to the point list
-  void addPoint(Real x, Real y, Real z);
+	/// Add a point to the point list
+	void addPoint(const Ogre::Vector3 &p);
+	/// Add a point to the point list
+	void addPoint(Real x, Real y, Real z);
 
-  /// Change the location of an existing point in the point list
-  void setPoint(unsigned short index, const Vector3 &value);
+	/// Change the location of an existing point in the point list
+	void setPoint(unsigned short index, const Vector3 &value);
 
-  /// Return the location of an existing point in the point list
-  const Vector3& getPoint(unsigned short index) const;
+	/// Return the location of an existing point in the point list
+	const Vector3& getPoint(unsigned short index) const;
 
-  /// Return the total number of points in the point list
-  unsigned short getNumPoints(void) const;
+	/// Return the total number of points in the point list
+	unsigned short getNumPoints(void) const;
 
-  /// Remove all points from the point list
-  void clear();
+	/// Remove all points from the point list
+	void clear();
 
-  /// Call this to update the hardware buffer after making changes.  
-  void update();
+	/// Call this to update the hardware buffer after making changes.  
+	void update();
 
-  /** Set the type of operation to draw with.
-   * @param opType Can be one of 
-   *    - RenderOperation::OT_LINE_STRIP
-   *    - RenderOperation::OT_LINE_LIST
-   *    - RenderOperation::OT_POINT_LIST
-   *    - RenderOperation::OT_TRIANGLE_LIST
-   *    - RenderOperation::OT_TRIANGLE_STRIP
-   *    - RenderOperation::OT_TRIANGLE_FAN
-   *    The default is OT_LINE_STRIP.
-   */
-  void setOperationType(OperationType opType);
-  OperationType getOperationType() const;
+	/** Set the type of operation to draw with.
+	* @param opType Can be one of 
+	*    - RenderOperation::OT_LINE_STRIP
+	*    - RenderOperation::OT_LINE_LIST
+	*    - RenderOperation::OT_POINT_LIST
+	*    - RenderOperation::OT_TRIANGLE_LIST
+	*    - RenderOperation::OT_TRIANGLE_STRIP
+	*    - RenderOperation::OT_TRIANGLE_FAN
+	*    The default is OT_LINE_STRIP.
+	*/
+	void setOperationType(OperationType opType);
+	OperationType getOperationType() const;
 
 protected:
-  /// Implementation DynamicRenderable, creates a simple vertex-only decl
-  virtual void createVertexDeclaration();
-  /// Implementation DynamicRenderable, pushes point list out to hardware memory
-  virtual void fillHardwareBuffers();
+	/// Implementation DynamicRenderable, creates a simple vertex-only decl
+	virtual void createVertexDeclaration();
+	/// Implementation DynamicRenderable, pushes point list out to hardware memory
+	virtual void fillHardwareBuffers();
 
 private:
-  std::vector<Vector3> mPoints;
-  bool mDirty;
+	std::vector<Vector3> mPoints;
+	bool mDirty;
 };
 
 
