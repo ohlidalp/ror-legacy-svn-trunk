5f0a8a90eb0ff058cca5f4457332c5d8e6d8b825
rorthomas <rorthomas@66ec8c48-a24b-4d9a-a200-91c837921668>
2011-06-28 16:31:00 +0000

    repo mode improvements

--- trunk/source/main/physics/input_output/SerializedRig.cpp
+++ trunk/source/main/physics/input_output/SerializedRig.cpp
@@ -5154,6 +5154,30 @@ int SerializedRig::loadTruck(String fname, SceneManager *manager, SceneNode *par
 		{
 			String fn = SSETTING("vehicleOutputFile") + ".jpg";
 
+
+			// calculate min camera radius for truck and the trucks center
+
+			// first: average pos
+			Vector3 aposition = Vector3::ZERO;
+			for (int i=0; i<free_node; i++)
+				aposition += nodes[i].AbsPosition;
+			aposition = aposition / free_node;
+
+			// then camera radius:
+			float minCameraRadius = 0;
+			for (int i=0; i<free_node; i++)
+			{
+				Real dist = nodes[i].AbsPosition.distance(aposition);
+				if(dist > minCameraRadius)
+					minCameraRadius = dist;
+			}
+			minCameraRadius *= 1.6f; // ten percent buffer
+
+			// position the camera in a good way
+			Camera *cam = RoRFrameListener::eflsingleton->getCamera();
+			cam->setPosition(Vector3(0, -minCameraRadius, 0.01f));
+			cam->lookAt(aposition);
+
 			// create some screenshot
 			RoRWindowEventUtilities::messagePump();
 			Ogre::Root::getSingleton().renderOneFrame();
