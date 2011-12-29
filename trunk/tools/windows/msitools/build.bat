b73c926fbc7e6dda45e18541f407779d8022c19f
rorthomas <rorthomas@66ec8c48-a24b-4d9a-a200-91c837921668>
2011-12-29 23:07:48 +0000

    removed some old folders plugins.cfg now loading DX10 and DX11 as well if possible improved SVN ignore lists

--- trunk/tools/windows/msitools/build.bat
+++
@@ -1,3 +0,0 @@
-"C:\Program Files (x86)\Windows Installer XML v3.5\bin\candle.exe" VCCRTVS2010.wxs -out VCCRTVS2010.wixobj
-"C:\Program Files (x86)\Windows Installer XML v3.5\bin\light.exe" VCCRTVS2010.wixobj -out VCCRTVS2010.msi   
-pause
