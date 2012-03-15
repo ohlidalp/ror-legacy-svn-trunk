0eebbc9d88ddd65b5b109e24651dae256215fffd
rorthomas <rorthomas@66ec8c48-a24b-4d9a-a200-91c837921668>
2012-03-15 22:03:08 +0000

    added silly wheel cam, WIP

---
+++ trunk/source/main/gfx/camera/CameraBehaviorWheelChase.h
@@ -0,0 +1,43 @@
+/*
+This source file is part of Rigs of Rods
+Copyright 2005-2012 Pierre-Michel Ricordel
+Copyright 2007-2012 Thomas Fischer
+
+For more information, see http://www.rigsofrods.com/
+
+Rigs of Rods is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, as
+published by the Free Software Foundation.
+
+Rigs of Rods is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Rigs of Rods.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef CAMERABEHAVIORWHEELCHASE_H__
+#define CAMERABEHAVIORWHEELCHASE_H__
+
+#include "RoRPrerequisites.h"
+#include "CameraBehavior.h"
+
+class CameraBehaviorWheelChase : public CameraBehavior
+{
+public:
+	void activate();
+	void deactivate();
+
+	void update(cameraContext_t &ctx);
+
+	bool mouseMoved(const OIS::MouseEvent& _arg);
+	bool mousePressed(const OIS::MouseEvent& _arg, OIS::MouseButtonID _id);
+	bool mouseReleased(const OIS::MouseEvent& _arg, OIS::MouseButtonID _id);
+
+	bool allowInteraction() { return false; };
+};
+
+#endif // CAMERABEHAVIORWHEELCHASE_H__
+
+
